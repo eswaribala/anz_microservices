74adb3b96b995609b7ee394cfa8b39441c7d3e0a
eswaribala <Parameswaribala@gmail.com>
2017-08-24 15:25:29 +0530

    Add files via upload

---
+++ FunctionExpression.js
@@ -0,0 +1,11 @@
+/**
+ * Created by BALASUBRAMANIAM on 24/08/2017.
+ */
+//Immediate Invocation of Function Expression
+invoke=function()
+{
+   for(i=0;i<100;i++)
+    console.log(Math.random())
+
+
+}();
