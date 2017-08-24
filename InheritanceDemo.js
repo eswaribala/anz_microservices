511727ea92263ef491dfbced716c658e780d8b0b
eswaribala <Parameswaribala@gmail.com>
2017-08-24 15:28:19 +0530

    Add files via upload

--- InheritanceDemo.js
+++ InheritanceDemo.js
@@ -1,6 +1,31 @@
 /**
  * Created by BALASUBRAMANIAM on 24/08/2017.
  */
+//javascript std
+function Person(name,address)
+{
+    this.Name=name;
+    this.Address=address;
+}
+function Member(name,address,type)
+{
+    Person.call(this,name,address);
+    this.Type=type;
+}
+function President(name,address,type,role)
+{
+    Member.call(this,name,address,type);
+    this.Role=role;
+}
+
+Member.prototype=new Person()
+President.prototype=new Member()
+obj=new President("Arun","Chennai","Gold","Admin")
+console.log(obj.Address,obj.Type,obj.Role)
+
+
+/*
+
 //Ecma standard
 
 class Person
@@ -36,3 +61,4 @@ class President extends Member{
 
 var object =new President('Sanjay','Chennai','gold','Admin');
 console.log(object);
+*/
