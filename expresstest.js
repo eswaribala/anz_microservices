910973d28029ca2282618df64f7ec1a06e703d2a
eswaribala <Parameswaribala@gmail.com>
2017-08-28 09:52:25 +0530

    Add files via upload

---
+++ expresstest.js
@@ -0,0 +1,45 @@
+/**
+ * Created by BALASUBRAMANIAM on 27/08/2017.
+ */
+var express   =    require("express");
+var mysql     =    require('mysql');
+var app       =    express();
+
+var pool      =    mysql.createPool({
+    connectionLimit : 100, //important
+    host     : 'localhost',
+    user     : 'root',
+    password : 'vignesh',
+    database : 'nessu',
+    debug    :  false
+});
+
+function handle_database(req,res) {
+
+    pool.getConnection(function(err,connection){
+        if (err) {
+            res.json({"code" : 100, "status" : "Error in connection database"});
+            return;
+        }
+
+        console.log('connected as id ' + connection.threadId);
+
+        connection.query("select * from employee",function(err,rows){
+            connection.release();
+            if(!err) {
+                res.json(rows);
+            }
+        });
+
+        connection.on('error', function(err) {
+            res.json({"code" : 100, "status" : "Error in connection database"});
+            return;
+        });
+    });
+}
+
+app.get("/",function(req,res){-
+    handle_database(req,res);
+});
+
+app.listen(4000);
